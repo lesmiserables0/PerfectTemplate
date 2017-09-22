567ed0a98ce177e7e3634ba63362bc6b0b6b7877
hubing <hubing@chengzhangdeiMac.local>
2017-09-22 17:34:50 +0800

    no message

--- Sources/main.swift
+++ Sources/main.swift
@@ -28,48 +28,70 @@ func handler(data: [String:Any]) throws -> RequestHandler {
 		request, response in
 		// Respond with a simple message.
 		response.setHeader(.contentType, value: "text/html")
-		response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+		response.appendBody(string: "<html><head><meta charset='utf-8'></head><title>欢饮来到我的笔记!</title><body>你好，我的朋友!</body></html>")
 		// Ensure that response.completed() is called when your processing is done.
 		response.completed()
 	}
 }
 
+
+let server = HTTPServer()
+server.serverName = "localhost"
+var routes = Routes()
+routes.add(method: .get, uri: "/login") { (request, response) in
+    response.setBody(string: "<html><head><meta charset='utf-8'></head><body><p>我是/login路asf径返回的信息</p></body></html>")
+    response.completed()
+}
+
+server.addRoutes(routes)
+
+server.serverPort = 8082
+
+do {
+try  server.start()
+}catch{
+    fatalError("\(error)");
+}
 // Configuration data for an example server.
 // This example configuration shows how to launch a server
 // using a configuration dictionary.
-
-
 let confData = [
-	"servers": [
-		// Configuration data for one server which:
-		//	* Serves the hello world message at <host>:<port>/
-		//	* Serves static files out of the "./webroot"
-		//		directory (which must be located in the current working directory).
-		//	* Performs content compression on outgoing data when appropriate.
-		[
-			"name":"localhost",
-			"port":8181,
-			"routes":[
-				["method":"get", "uri":"/", "handler":handler],
-				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
-				 "documentRoot":"./webroot",
-				 "allowResponseFilters":true]
-			],
-			"filters":[
-				[
-				"type":"response",
-				"priority":"high",
-				"name":PerfectHTTPServer.HTTPFilter.contentCompression,
-				]
-			]
-		]
-	]
+    "servers": [
+        // Configuration data for one server which:
+        //    * Serves the hello world message at <host>:<port>/
+        //    * Serves static files out of the "./webroot"
+        //        directory (which must be located in the current working directory).
+        //    * Performs content compression on outgoing data when appropriate.
+        [
+            "name":"localhost",
+            "port":8081,
+            "routes":[
+                ["method":"get", "uri":"/", "handler":handler],
+                ["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
+                 "documentRoot":"./webroot",
+                 "allowResponseFilters":true]
+            ],
+            
+            "filters":[
+                [
+                "type":"response",
+                "priority":"high",
+                "name":PerfectHTTPServer.HTTPFilter.contentCompression,
+                ]
+            ]
+        ]
+    ]
 ]
 
 do {
-	// Launch the servers based on the configuration data.
-	try HTTPServer.launch(configurationData: confData)
+    // Launch the servers based on the configuration data.
+    try HTTPServer.launch(configurationData: confData)
 } catch {
-	fatalError("\(error)") // fatal error launching one of the servers
+    fatalError("\(error)") // fatal error launching one of the servers
 }
+
+
+
+
+
 
